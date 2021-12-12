6ea2b7be57409aa8e09d7c7f3ba4243bccfbceb6
Andreea-Elena <andreea.elena098@gmail.com>
2021-12-12 12:04:42 +0200

    first commit

---
+++ Calculation.java
@@ -0,0 +1,16 @@
+public class Calculation {
+
+	public static int findMax(int arr[]) {
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i])
+				max = arr[i];
+		}
+		return max;
+	}
+
+	public static int cube(int n) {
+		return n * n * n;
+	}
+
+}
